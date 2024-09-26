ed09e8d819a346cebb776444fed0baeedb8613a7
mohammedyousuf051 <mohammedyousuf051@gmail.com>
2024-09-26 20:54:48 +0800

    added refernce script

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -11,7 +11,7 @@ export const appConfig: ConnectionOptions = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: true,
+    synchronize: true, // SET TO FALSE IN PRODUCTION
     logging: false,
     entities: [Draw, Participant],
     migrations: [],
